280660f789e1d6e079bc87291abd36fcb55215a6
Dharav Bhatt <62647174+Dnbhatt99@users.noreply.github.com>
2023-08-27 15:37:06 -0500

    GROUP BY/DISTINCT

---
+++ 18_Reported Posts_1113.sql
@@ -0,0 +1,6 @@
+-- https://leetcode.com/problems/reported-posts/description/
+
+SELECT extra AS report_reason, count(DISTINCT(post_id)) AS report_count
+  FROM Actions
+  WHERE action_date = '2019-07-04' AND action='report'
+  GROUP BY extra
