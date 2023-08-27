18f0aa24c586b32f5417891dcbcf88719a016af4
Dharav Bhatt <62647174+Dnbhatt99@users.noreply.github.com>
2023-08-27 15:27:38 -0500

    GROUP BY on more than one variable

---
+++ 11_Find Total Time Spent by Each Employee_1741.sql
@@ -0,0 +1,6 @@
+-- https://leetcode.com/problems/find-total-time-spent-by-each-employee/
+
+-- GROUP BY on more than one variable
+select event_day as day, emp_id, SUM(out_time - in_time) as total_time
+from Employees
+group by emp_id, event_day
